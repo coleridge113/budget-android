ba75699a915ddf0cbeae9c0cb297d505f0807161
Jose Luna <josemanuelmluna@gmail.com>
2025-11-06 11:55:24 +0800

    refactor: removed duplicate files

--- app/src/main/java/com/luna/budgetapp/presentation/AddExpenseViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.luna.budgetapp.presentation
-
-import com.luna.budgetapp.domain.usecase.expense.AddExpenseUseCase
-
-class AddExpenseViewModel(
-    private val addExpenseUseCase: AddExpenseUseCase
-) {
-}

5cd8cfca2135402f1e2fd00e6df1f2a2ac32112b
Jose Luna <josemanuelmluna@gmail.com>
2026-01-03 14:58:44 +0800

    fix: added serialized names to dto

--- app/src/main/java/com/luna/budgetapp/data/remote/dto/AuthDto.kt
+++ app/src/main/java/com/luna/budgetapp/data/remote/dto/AuthDto.kt
@@ -1,8 +1,15 @@
 package com.luna.budgetapp.data.remote.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class AuthRequest(
     val username: String, 
     val password: String
 ) 
 
-data class AuthResponse(val token: String)
+data class AuthResponse(
+    @SerializedName("accessToken")
+    val token: String,
+    @SerializedName("tokenType")
+    val type: String
+)
--- app/src/main/java/com/luna/budgetapp/data/utils/PusherManager.kt
+++ app/src/main/java/com/luna/budgetapp/data/utils/PusherManager.kt
@@ -1,11 +1,11 @@
 package com.luna.budgetapp.data.utils
 
 import com.luna.budgetapp.BuildConfig
-import com.luna.budgetapp.domain.repository.AuthRepository
-import com.luna.budgetapp.domain.usecase.UseCases
+import com.luna.budgetapp.data.datastore.AuthLocalDataSource
 import com.pusher.client.Pusher
 import com.pusher.client.PusherOptions
 import com.pusher.client.channel.PrivateChannelEventListener
+import com.luna.budgetapp.domain.repository.AuthRepository
 
 class PusherManager(
     private val repository: AuthRepository
@@ -13,16 +13,14 @@ class PusherManager(
     private val key = BuildConfig.PUSHER_KEY
     private val cluster = BuildConfig.PUSHER_CLUSTER
     private val authEndpoint = "${BuildConfig.LOCAL_BACKEND_URL}/api/v1/pusher/auth"
-
     private val privateExpenseChannel = "private-expense-channel"
     private val expenseEventName = "expense-added"
-
     private var pusher: Pusher? = null
 
     suspend fun initPusher() {
-        if (pusher != null) return // already initialized
+        if (pusher != null) return
 
-        val jwtToken = repository.getJwtToken() ?: throw IllegalStateException("JWT missing")
+        val jwtToken = repository.fetchJwtToken() ?: throw IllegalStateException("JWT missing")
         val options = PusherOptions().apply {
             setCluster(cluster)
             setUseTLS(true)

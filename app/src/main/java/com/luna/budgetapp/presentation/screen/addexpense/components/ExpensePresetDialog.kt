364866b01577240efc194ba2102b911b15fb3c98
Jose Luna <josemanuelmluna@gmail.com>
2026-02-03 22:00:12 +0800

    feat: added dialog for expense presets

--- app/src/main/java/com/luna/budgetapp/presentation/screen/addexpense/components/ExpensePresetDialog.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/addexpense/components/ExpensePresetDialog.kt
@@ -1,51 +1,45 @@
 package com.luna.budgetapp.presentation.screen.addexpense.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import android.annotation.SuppressLint
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MenuDefaults
-import androidx.compose.material3.MenuDefaults.itemColors
-import androidx.compose.material3.MenuItemColors
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpensePresetDialog(
     onDismissRequest: () -> Unit,
+    onConfirm: (String, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
 
@@ -53,93 +47,80 @@ fun ExpensePresetDialog(
         onDismissRequest = onDismissRequest,
         modifier = modifier,
     ) {
-        var categoryExpanded by remember { mutableStateOf(false) }
-        var typeExpanded by remember { mutableStateOf(false) }
-        var selectedCategory by remember { mutableStateOf("Selected Category")}
+        val options = listOf("Food", "Coffee", "Commute")
+        var expanded by remember { mutableStateOf(false) }
+        var selectedOption by remember { mutableStateOf(options[0])}
+        var amountState = rememberTextFieldState("")
 
-        Box(modifier = Modifier.wrapContentSize()) {
-            OutlinedButton(onClick = { categoryExpanded = true }) {
-                Text(selectedCategory)
-                Icon(imageVector = Icons.Default.Add, contentDescription = null)
-            }
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            tonalElevation = 6.dp,
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = "Add Expense Preset",
+                    style = MaterialTheme.typography.headlineSmall
+                )
 
-            DropdownMenu(categoryExpanded, onDismissRequest) {
-                DropdownMenuItem(
-                    text = { Text("Food") },
-                    onClick = {
-                        selectedCategory = "Food"
-                        categoryExpanded = false
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded }
+                ) {
+                    TextField(
+                        value = selectedOption,
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text("Select category")},
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                        modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable, true)
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false}
+                    ) {
+                        options.forEach { option ->
+                            DropdownMenuItem(
+                                text = { Text(option) },
+                                onClick = {
+                                    selectedOption = option
+                                    expanded = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
                     }
+                }
+
+                OutlinedTextField(
+                    state = amountState,
+                    label = { Text("Amount")}
                 )
-                DropdownMenuItem(
-                    text = { Text("Commute") },
-                    onClick = {
-                        selectedCategory = "Commute"
-                        categoryExpanded = false
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextButton(onClick = onDismissRequest) {
+                        Text("Cancel")
                     }
-                )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = {
+                        onConfirm(selectedOption, amountState.text.toString())
+                    }) {
+                        Text("Confirm")
+                    }
+
+                }
             }
         }
     }
-}
-
-
-@Composable
-@SuppressLint("ComposableNaming")
-fun DropdownMenu(
-    expanded: Boolean,
-    onDismissRequest: () -> Unit,
-    modifier: Modifier = Modifier,
-    offset: DpOffset = DpOffset(0.dp, 0.dp),
-    scrollState: ScrollState = rememberScrollState(),
-    properties: PopupProperties = PopupProperties(focusable = true),
-    content: @Composable ColumnScope.() -> Unit,
-) =
-    DropdownMenu(
-        expanded = expanded,
-        onDismissRequest = onDismissRequest,
-        modifier = modifier,
-        offset = offset,
-        scrollState = scrollState,
-        properties = properties,
-        shape = MenuDefaults.shape,
-        containerColor = MenuDefaults.containerColor,
-        tonalElevation = MenuDefaults.TonalElevation,
-        shadowElevation = MenuDefaults.ShadowElevation,
-        border = null,
-        content = content,
-    )
-
-@Composable
-fun DropdownMenuItem(
-    text: @Composable () -> Unit,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    enabled: Boolean = true,
-    colors: MenuItemColors = MenuDefaults.itemColors(),
-    contentPadding: PaddingValues = MenuDefaults.DropdownMenuItemContentPadding,
-    interactionSource: MutableInteractionSource? = null,
-) {
-    ListItem(
-        headlineContent = text,
-        modifier = modifier
-            .clickable(
-                enabled = enabled,
-                onClick = onClick,
-                interactionSource = interactionSource,
-                indication = ripple()
-            )
-            .fillMaxWidth(),
-        leadingContent = leadingIcon,
-        trailingContent = trailingIcon,
-        colors = ListItemDefaults.colors(
-            containerColor = Color.Transparent,
-            headlineColor = if (enabled) itemColors().textColor else itemColors().disabledTextColor,
-            leadingIconColor = if (enabled) itemColors().leadingIconColor else itemColors().disabledLeadingIconColor
-        )
-    )
 }
 
 @Preview(
@@ -151,6 +132,9 @@ fun DropdownMenuItem(
 fun ExpensePresetDialogPreview() {
     Spacer(modifier = Modifier.height(50.dp))
     Box(modifier = Modifier.fillMaxSize()){
-        ExpensePresetDialog(onDismissRequest = {})
+        ExpensePresetDialog(
+            onDismissRequest = {},
+            onConfirm = { _, _ -> }
+        )
     }
 }

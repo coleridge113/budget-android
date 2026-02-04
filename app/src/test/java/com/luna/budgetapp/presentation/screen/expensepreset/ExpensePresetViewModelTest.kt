862a79d3a24bc159403e7adf51ca9293900ae5e3
coleridge113 <josemanuelmluna@gmail.com>
2026-02-04 18:12:01 +0800

    feat: added main dispatcher rule and test viewmodel

---
+++ app/src/test/java/com/luna/budgetapp/presentation/screen/expensepreset/ExpensePresetViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.luna.budgetapp.presentation.screen.expensepreset
+
+import com.luna.budgetapp.MainDispatcherRule
+import org.junit.Rule
+
+class ExpensePresetViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+}
+

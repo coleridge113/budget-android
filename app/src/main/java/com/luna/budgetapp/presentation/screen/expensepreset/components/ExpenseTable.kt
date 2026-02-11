9010fe5c3722dc65e89722b5cc5fa13bc8e03da5
Jose Luna <josemanuelmluna@gmail.com>
2026-02-11 18:01:58 +0800

    feat: updated UI for expense table

--- app/src/main/java/com/luna/budgetapp/presentation/screen/expensepreset/components/ExpenseTable.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/expensepreset/components/ExpenseTable.kt
@@ -8,13 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 import com.luna.budgetapp.domain.model.ExpensePreset
 import com.luna.budgetapp.ui.icons.CoffeeIcon
 import com.luna.budgetapp.ui.icons.CirclePlusIcon
@@ -52,17 +61,26 @@ fun ExpenseItem(
 ) {
     Row(
         modifier = Modifier.fillMaxWidth()
-            .clickable { onClick(item) },
+            .clickable { onClick(item) }
+            .padding(vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.weight(1f)
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)),
             contentAlignment = Alignment.Center
         ) {
             Icon(imageVector = icon, contentDescription = null)
         }
+        Spacer(Modifier.width(4.dp))
         Box(
-            modifier = Modifier.weight(3f),
+            modifier = Modifier.weight(3f)
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f))
+            ,
             contentAlignment = Alignment.Center
         ) {
             Text(text = "${item.type} - P${item.amount}")

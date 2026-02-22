c27f689c2771803a0655552a61e28481f6863980
Jose Luna <josemanuelmluna@gmail.com>
2026-02-22 15:07:54 +0800

    feat: added top app bar back navigation in expense list screen

--- app/src/main/java/com/luna/budgetapp/presentation/screen/expenselist/ExpenseListRoute.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/expenselist/ExpenseListRoute.kt
@@ -3,15 +3,23 @@ package com.luna.budgetapp.presentation.screen.expenselist
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.luna.budgetapp.presentation.screen.expenselist.components.ExpenseTable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpenseListRoute(
     navController: NavController,
@@ -21,7 +29,25 @@ fun ExpenseListRoute(
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier,
+                title = {},
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        }
     ) { innerPadding ->
         MainContent(
             modifier = Modifier.padding(innerPadding),
